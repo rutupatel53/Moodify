1d656f1659c5c82d2cd9889d41d114f04db33961
Rutu patel <rutupatel053@gmail.com>
2024-01-02 01:24:44 +0530

    added Calendar

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import homeimg from "../Assets/homepage.jpg";
+import Calendar from "../Component/Calendar/Calendar";
 
 export const Home = () => {
   return (
     <div className="Home">
-      <div>
-        <img
-          src={homeimg}
-          alt="Homepage"
-          className=" w-1/2 h-96 pt-2 pb-24 pl-10 rounded-full"
-        />
+      <div className="float-right mr-96 border-r-4 w-0.5 border-black-100 h-screen p-4">
+        <div className="w-96 h-96">
+          <Calendar />
+        </div>
       </div>
+
+      <img
+        src={homeimg}
+        alt="Homepage"
+        className="w-1/2 h-96 pt-4 pb-24 pl-24 rounded-full"
+      />
     </div>
   );
 };
