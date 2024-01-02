f27e4e7805ba9bb8d68e45e3884ab71c82af632c
Rutu patel <rutupatel053@gmail.com>
2024-01-03 01:13:08 +0530

    added icons

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../Assets/moodifylogo.png";
+import HomeIcon from "../../Assets/HomeIcon.png";
+import CalendarIcon from "../../Assets/CalendarIcon.png";
+import GoalIcon from "../../Assets/Goalicon.png";
 export const Navbar = () => {
   const [menu, setMenu] = useState("Home");
   const navigate = useNavigate();
@@ -16,7 +19,8 @@ export const Navbar = () => {
             navigate("/");
           }}
         >
-          Home
+          <img src={HomeIcon} className="w-10 h-10 float-start mb-4 " />
+          <p className="mt-2 ml-9">Home</p>
         </li>
         <li
           className="cursor-pointer hover:bg-gray-300 p-2 rounded"
@@ -25,7 +29,8 @@ export const Navbar = () => {
             navigate("/Calendar");
           }}
         >
-          Calendar
+          <img src={CalendarIcon} className="w-8 h-8 float-start mb-4 " />{" "}
+          <p className="ml-9 mt-1">Calendar</p>
         </li>
         <li
           className="cursor-pointer hover:bg-gray-300 p-2 rounded"
@@ -34,7 +39,8 @@ export const Navbar = () => {
             navigate("/Goals");
           }}
         >
-          Goals
+          <img src={GoalIcon} className="w-8 h-8 float-start mb-4 " />
+          <p className="ml-9 mt-1"> Goals</p>
         </li>
       </ul>
     </div>
